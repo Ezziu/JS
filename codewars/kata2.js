3b1155c2c7a3eca97f8a9ca1c38e4220139dfb15
Ezziu <arzetelski@gmail.com>
2018-03-06 23:52:45 +0100

    jup

---
+++ codewars/kata2.js
@@ -0,0 +1,16 @@
+/*function songDecoder(song){
+    console.log('dupa');
+ let title = song.toArray();
+ for (i = 0; i <song.length ; i++){
+  if (song[i] === "W" && song[i+1] === "U" && song[i+2] === "B" )
+   i += 3;
+  else 
+  title.push(song[i]);
+ };
+  return title.toString();  
+}
+*/
+function songDecoder(song){
+    console.log("dupa");
+};
+
